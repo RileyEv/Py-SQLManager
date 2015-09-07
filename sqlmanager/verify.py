41be08e42146cf5b65728fef490b6d960a35e719
Riley Evans <14.rileyevans@gmail.com>
2015-09-07 10:26:37 +0100

    Update verify.py

--- sqlmanager/verify.py
+++ sqlmanager/verify.py
@@ -2,15 +2,18 @@
 
 
 class Verify():
-    def __init__(self, table, query, fetch=['*']):
+    def __init__(self, table, query, fetch=['*'], values={}):
         self._table = table
         self._query = query
         self._fetch = fetch
+        self._values = values
         self._table_valid = False
         self._query_valid = False
         self._table_valid = False
+        self._values_valid = False
         verify_table()
-        verify_query()
+        self._query_valid = verify_query(self._query)
+        self._values_valid = verify_query(self._values)
         verify_fetch()
     
     def verify_table(self):
@@ -19,18 +22,21 @@ class Verify():
         else:
             raise TypeError('The table needs to be a string')
     
-    def verify_query(self):
-        if type(self._query).__name__ == 'dict':
+    def verify_query(self, query):
+        if type(query).__name__ == 'dict':
             for k, v in query:
                 if type(v).__name__ == 'dict':
                     for a, b in v:
                         if type(b).__name__ != 'str':
                             raise TypeError('You have entered your query in an incorrect format')
+                            return False
                 else:
                     raise TypeError('You have entered your query in an incorrect format')
-            self._query_valid = True
+                    return False
+            return True
         else:
             raise TypeError('The query needs to be a dict')
+            return False
     
     def verify_fetch(self):
         if type(self._fetch).__name__ == 'list':
@@ -46,7 +52,7 @@ class Verify():
             raise TypeError('The fetch needs to be a list')
             
     def valid(self):
-        if self._table_valid and self._query_valid and self._fetch_valid:
+        if self._table_valid and self._query_valid and self._fetch_valid and self._values_valid:
             return True
         else:
             return False
