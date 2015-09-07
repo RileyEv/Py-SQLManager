d8d8ebcaa5ff2b9c43e5ee67cfe0bc4f5bae8aad
Riley Evans <14.rileyevans@gmail.com>
2015-09-07 10:16:51 +0100

    Update insert.py

--- sqlmanager/insert.py
+++ sqlmanager/insert.py
@@ -3,36 +3,36 @@
 from .verify import Verify
 
 class Select():
-    def __init__(self, table, query):
-        verify = Verify(table, query)
+    def __init__(self, table, values):
+        verify = Verify(table, values)
         if verify.valid():
             self._table = table
-            self._query = query
+            self._values = values
         self.sql = self.sql_gen()
     
     def _dict_to_list(self):
         new_list = []
-        for k,v in self._query:
+        for k,v in self._values:
             new_list.append([k,v])
         self._query = new_list
         
     def sql_gen(self):
         sql = 'INSERT INTO ' + self._table
-        sql += self.query_gen()
+        sql += self.values_gen()
         return sql
 
-    def query_gen(self):
+    def values_gen(self):
         query = ''
         count = 0
         query += '('
-        for i in self._query:
+        for i in self._values:
             if count == 0:
                 query += i[0]
             else:
                 query += ',' + i[0]
         query += ') VALUES ('
         count = 0
-        for i in self._query:
+        for i in self._values:
             if count == 0:
                 query += i[1]
             else:
