d6c6b8bde8ec6e182541245dc4afd4ffc000f6de
Riley Evans <14.rileyevans@gmail.com>
2015-09-07 10:14:43 +0100

    moved validation to the verify.py file

--- sqlmanager/select.py
+++ sqlmanager/select.py
@@ -4,32 +4,11 @@ from .verify import Verify
 
 class Select():
     def __init__(self, table, query, fetch=['*']):
-        if type(fetch).__name__ == 'list':
-            for i in fetch:
-                if type(i).__name__ != 'str':
-                    raise TypeError(
-                        "Only strings allowed in the fetch list. This isnt a string '{0}'".format(
-                            i
-                        )
-                    )
-            self._fetch = fetch
-        else:
-            raise TypeError('The fetch needs to be a list')
-        if type(query).__name__ == 'dict':
-            for k, v in query:
-                if type(v).__name__ == 'dict':
-                    for a, b in v:
-                        if type(b).__name__ != 'str':
-                            raise TypeError('You have entered your query in an incorrect format')
-                else:
-                    raise TypeError('You have entered your query in an incorrect format')
-            self._query = query
-        else:
-            raise TypeError('The query needs to be a dict')
-        if type(table).__name__ == 'str':
+        verify = Verify(table, query, fetch)
+        if verify.valid():
             self._table = table
-        else:
-            raise TypeError('The table needs to be a string')
+            self._query = query
+            self._fetch = fetch
         self._sql = self.sql_gen()
 
     def sql_gen(self):
