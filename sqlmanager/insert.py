4bef4e62886b8a0d9ecf701b06cbfb8b48986808
Riley Evans <14.rileyevans@gmail.com>
2015-09-07 10:11:03 +0100

    converted old select.py model to handle insert queries

--- sqlmanager/insert.py
+++ sqlmanager/insert.py
@@ -1,72 +1,40 @@
 # -*- coding: utf-8 -*-
 
+from .verify import Verify
 
 class Select():
-    def __init__(self, table, query, fetch=['*']):
-        if type(fetch).__name__ == 'list':
-            for i in fetch:
-                if type(i).__name__ != 'str':
-                    raise TypeError(
-                        "Only strings allowed in the fetch list. This isnt a string '{0}'".format(
-                            i
-                        )
-                    )
-            self._fetch = fetch
-        else:
-            raise TypeError('The fetch needs to be a list')
-        if type(query).__name__ == 'dict':
-            for k, v in query:
-                if type(v).__name__ == 'dict':
-                    for a, b in v:
-                        if type(b).__name__ != 'str':
-                            raise TypeError('You have entered your query in an incorrect format')
-                else:
-                    raise TypeError('You have entered your query in an incorrect format')
-            self._query = query
-        else:
-            raise TypeError('The query needs to be a dict')
-        if type(table).__name__ == 'str':
+    def __init__(self, table, query):
+        verify = Verify(table, query)
+        if verify.valid():
             self._table = table
-        else:
-            raise TypeError('The table needs to be a string')
-        self._sql = self.sql_gen()
-
+            self._query = query
+        self.sql = self.sql_gen()
+    
+    def _dict_to_list(self):
+        new_list = []
+        for k,v in self._query:
+            new_list.append([k,v])
+        self._query = new_list
+        
     def sql_gen(self):
-        sql = 'SELECT ' + self.fetch_gen()
-        sql += ' FROM {}'.format(self._table)
-        sql += ' WHERE ' + self.query_gen()
+        sql = 'INSERT INTO ' + self._table
+        sql += self.query_gen()
         return sql
 
-    def fetch_gen(self):
-        fetch = ''
-        for i in range(len(self._fetch)):
-            if i == 0:
-                fetch += '{}'.format(self._fetch[i])
-            else:
-                fetch += ',{}'.format(self._fetch[i])
-        return fetch
-
     def query_gen(self):
-        operators = {
-            'equal': '{}={}',
-            'notequal': '{}!={}',
-            'greater': '{}>{}',
-            'greaterequal': '{}>={}',
-            'less': '{}<{}',
-            'lessequal': '{}<{}',
-            'like': '{} LIKE {}',
-        }
         query = ''
-        for k, v in self._query:
-            for a, b in v:
-                if query == '':
-                    try:
-                        query += operators[k].format(a, b)
-                    except:
-                        raise KeyError("'{}' is not a valid operator".format(k))
-                else:
-                    try:
-                        query += ' AND ' + operators[k].format(a, b)
-                    except:
-                        raise KeyError("'{}' is not a valid operator".format(k))
+        count = 0
+        query += '('
+        for i in self._query:
+            if count == 0:
+                query += i[0]
+            else:
+                query += ',' + i[0]
+        query += ') VALUES ('
+        count = 0
+        for i in self._query:
+            if count == 0:
+                query += i[1]
+            else:
+                query += ',' + i[1]
         return query
