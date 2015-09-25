cd7dfe41a33bb6500b9b9e2400b8da5eb252609c
Riley Evans - GaiGO <riley@gaigo.org>
2015-09-25 20:17:56 +0100

    Update db.py

--- db.py
+++ db.py
@@ -17,25 +17,21 @@ class Connection():
 
     def select(self, query, table, fetch=['*'], in_list=False):
         Query = select.Select(table, query, fetch)
-        print(Query._sql)
         return self.sql_action(Query._sql, need_response=True, in_list=in_list)
     
     def insert(self, table, values, in_list=False):
         Query = insert.Insert(table, values)
-        self.sql_action(Query._sql, need_response=True, in_list=in_list)
+        return self.sql_action(Query._sql, need_response=False, in_list=in_list)
         
     def update(self, table, query, values, in_list=False):
         Query = update.Update(table, query, values)
-        self.sql_action(Query._sql, need_response=True, in_list=in_list)
+        return self.sql_action(Query._sql, need_response=False, in_list=in_list)
 
     def sql_action(self, sql, need_response=False, in_list=False):
         self.cursor.execute(sql)
-        #cursor = self.cursor
-        #cursor.execute(sql)
-        response = ''
+        self._response = ''
         if need_response:
             self._response = self.cursor.fetchall()
-            #response = cursor.fetchall()
         if in_list:
             self._response = self._tupletolist(self._response)
         return self._response
