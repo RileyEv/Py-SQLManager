43d3ab6602e90e81072ce78429bcfc74f4d328d2
Riley Evans - GaiGO <riley@gaigo.org>
2015-10-31 19:53:48 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PySQLManager',
+    version='1.0',
+    description='MySQL connection manager',
+    author='RilCraft',
+    author_email='riley@gaigo.org',
+    url='https://github.com/RileyEv/Py-SQLManager/',
+    packages=['distutils', 'distutils.command'],
+)
